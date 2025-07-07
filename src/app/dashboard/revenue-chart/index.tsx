1512514e6f58e2118897cccd2fd5695ae75b80c6
GuilhermeVRocha <guilhermevieirar14@gmail.com>
2025-07-06 21:59:10 -0300

    create dinamic graph bar

--- src/app/dashboard/revenue-chart/index.tsx
+++ src/app/dashboard/revenue-chart/index.tsx
@@ -13,33 +13,35 @@ const graphData = [
   { month: 'Dez', value: 2000 },
 ]
 
+const params = [2000, 1000, 500, 250, 0]
+
 export function RevenueChart() {
   const dataProcess = graphData.map((item) => ({
     ...item,
-    percentual: ((item.value / 2000) * 100).toFixed(1) + '%',
+    percentual:
+      (item.value / Math.max(...graphData.map((a) => a.value))) * 100 + '%',
   }))
 
   return (
-    <div className="rounded-[0.75rem] p-10 w-full bg-white ">
+    <div className="rounded-[0.75rem] p-10 w-full overflow-hidden bg-white ">
       <p className="text-xl">Gráfico de receitas</p>
 
-      <div className="flex gap-6 mt-6 h-[84%]">
-        <div className="text-gray10 flex flex-col gap-6">
-          <p>2000</p>
-          <p>1000</p>
-          <p>500</p>
-          <p>250</p>
-          <p>0</p>
+      <div className="flex gap-7 mt-6 h-full">
+        <div className="text-gray10 flex flex-col justify-between h-[79.5%]">
+          {params.map((value) => (
+            <p key={value.toString()}>{value}</p>
+          ))}
         </div>
 
-        <div className="flex text-gray10 gap-3 justify-between w-full h-auto">
+        <div className="flex text-gray10 gap-4 w-full overflow-x-auto h-[89.5%]">
           {dataProcess.map(({ month, percentual }) => (
             <div
               key={month}
               className="flex flex-col h-full justify-end-safe items-center-safe w-[8.4%] gap-3"
             >
               <div
-                className={`h-[${percentual}] bg-brandColor rounded-md w-full}`}
+                style={{ height: percentual }}
+                className="bg-brandColor rounded-md w-full"
               />
               <p>{month}</p>
             </div>
